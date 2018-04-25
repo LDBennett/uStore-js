6b1963450e019511f6e10a8960022518e13d266d
Lee Bennett <LDBennett@gmail.com>
2018-04-25 08:55:20 -0400

    Update and rename productCustomization to productCustomization.js

--- productCustomization.js
+++ productCustomization.js
@@ -1,15 +1,14 @@
+<script>
 //Use this function to customize and target specific products in your store
 //You can get product IDs from the uStore menu in the ID column
 
-
 $(function() {
-	//Individual Product customizations
-	productCustomization(xmp.uStore.storeFrontParams.productId);
-  
+//Individual Product customizations
+productCustomization(xmp.uStore.storeFrontParams.productId);
+
   //Product customization function
-	//Takes the parameter product ID number and evaluates it with if statements
-	function productCustomization(currentProduct){
-  
+  //Takes the parameter product ID number and evaluates it with if statements
+  function productCustomization(currentProduct){
     //IF product is equal to the ID1
     if (currentProduct == ###1){
       //Do something here for this product
@@ -21,3 +20,5 @@ $(function() {
     }
    }
 });
+
+</script>
